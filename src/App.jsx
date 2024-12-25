70c695e75a2e8c47cfa533a18957d9dc906031b0
Coder3609 <d2424181@gmail.com>
2024-12-25 15:24:34 +0530

    made new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Portfolio from './Portfolio';
 import Alert from 'react-bootstrap/Alert';
 import Carousel from 'react-bootstrap/Carousel';
+
+import {slides} from "./data/carouselData.json"
 function App() {
 
 
